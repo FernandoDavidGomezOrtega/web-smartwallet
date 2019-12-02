1dce185b546b438bc21b1eae277273c2ec773058
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2019-12-02 13:23:40 +0100

    Revert "creada la estructura base del MVC. Todavía no se ve nada en la página…"

--- config/parameters.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define ('base_url', 'http://localhost/web-smartwallet');
-define ('controller_default', 'productoController');
-define ('action_default', 'index');
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once 'autoload.php';
-
-function show_error(){
-  $error = new errorController();
-  $error->index();
-}
-
